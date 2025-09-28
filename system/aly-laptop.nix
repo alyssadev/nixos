22a349a7a9780071df81bad7cabccbd6cb3df49a
alydev <alyssa.dev.smith@gmail.com>
2025-09-28 15:25:36 +1000

    update

--- system/aly-laptop.nix
+++ system/aly-laptop.nix
@@ -6,7 +6,7 @@
   fileSystems."/mnt/media" = {
     device = "//10.8.0.4/media";
     fsType = "cifs";
-    options = ["ro,x-systemd.requires=openvpn-laptop.service,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"];
+    options = ["x-systemd.requires=openvpn-laptop.service,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"];
   };
   services.logind = {
     lidSwitch = "suspend";
